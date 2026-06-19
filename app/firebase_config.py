0f99c5a9a56d6524b30660ee05337653650dd9d5
hassyb <hassonshareef@gmail.com>
2026-06-19 01:27:32 -0400

    Remove Firebase config and initialization
    Removed Firebase initialization code and configuration.

--- app/firebase_config.py
+++ app/firebase_config.py
@@ -6,9 +6,6 @@ firebaseConfig = {
     "messagingSenderId": "832029650032",
     "appId": "1:832029650032:web:caef5a08835a2c0b1558b5",
     "measurementId": "G-P9RQLPRZ5Q"
-}
-
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred)
+{
 
 
